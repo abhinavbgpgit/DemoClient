54b5a1bfd3ec9541c0adb176f227a190bef43749
abhinabgp <87983611+abhinabgp@users.noreply.github.com>
2022-02-02 15:54:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Users from './components/User Information/Users';
+import Foojer from './Foojer';
+
 
 function App() {
   return (
     <div className="App">
    <Users/>
+   
+   
     </div>
   );
 }
--- src/components/User Information/Users.js
+++ src/components/User Information/Users.js
@@ -1,91 +1,169 @@
-import React from 'react'
-import {useState} from 'react'
-import DatePicker from "react-datepicker";
 
-import "react-datepicker/dist/react-datepicker.css";
+import React, { useEffect } from 'react'
+import {useState} from 'react'
 import './users.css'
+import axios from "axios";
+import {IoTrashOutline } from "react-icons/io5";
+import { FiEdit } from "react-icons/fi";
+
+
+
 const Users = () => {
-    const [startDate, setStartDate] = useState("");
-    const [user, setUser]=useState({
-        fname:"", lname :"",bday:"", email :"" })
-        let name, value;
-       
+   
+    const [user, setUser]=useState({ID:"",firstName:"", lastName :"",dateOfBirth:"", email :"" })
+    const [allUser, setAllUser]=useState([])
+    const[toggle, setToggle]=useState(true)
+    // const[idno, setIdNo]=useState(0)
+    //const [userWithID,setUserWithId]=useState({id:"",firstName:"", lastName :"",dateOfBirth:"", email :"" })
+    
+    //****** storing input fields in hook user ******/
+    let name, value;   
         const handleInputs=(e)=>
         {
-            console.log(user)
-            name=e.target.name;
+             name=e.target.name;
             value=e.target.value;
-            setUser({...user,[name]:value});
-        }
-        const postData=(e)=>
+            setUser({...user,[name]:value});          
+        }   
+
+        const getdata=()=>
         {
-            console.log("hmm")
-            setUser({fname:"", lname :"",bday:"", email :""});
-            setStartDate("")
+          axios.get("http://localhost:8080/")
+          .then(response => {
+           setAllUser(response.data)
+                            })      
+         .catch(err => {console.log(err)}) 
+       console.log("hhhhhhhhhh")
         }
+
+ //****** Showing data on page on first render ******/
+        useEffect(()=>
+        {
+
+        axios.get("http://localhost:8080/")
+        .then(response => {
+         
+         setAllUser(response.data)
+         }).catch(err => {console.log(err)}) 
+            
+  },[]);
+
+
+
+  //****** Call POST API    ******/
+  
+    const postData = async(event) =>
+        {
+console.log("postdata")
+
+const final={ firstName:user.firstName, lastName :user.lastName,dateOfBirth:user.dateOfBirth, email :user.email}
+          event.preventDefault();
+          await axios.post("http://localhost:8080/Students",final)
+          .then(response => {
+        
+                })
+         .catch(err => {console.log(err)})          
+         setUser({ID:"",firstName:"", lastName :"",dateOfBirth:"", email :"" })
+         getdata();
+        getdata();    
+        }
+
+
+
+
+
+ // *****  DELETE USER  ********//    
+
+     const deleteUser=(del)=>
+            {               
+               axios.delete(`http://localhost:8080/Students/${del}`)
+               getdata();
+               getdata();                        
+          }
+
+
+// ******* FILL USER FORM TO UPDATE ******** //
+  
+ const FillUserForm=(i,a,b,c,d)=>
+          {
+           setUser({ID:i,firstName:a, lastName :b,dateOfBirth:c, email :d })
+           setToggle(false)
+            }
+  
+  // *****  UPDATE USER  ********//    
+
+          const updateUser=()=>
+          {           
+             const final={ firstName:user.firstName, lastName :user.lastName,dateOfBirth:user.dateOfBirth, email :user.email}
+             axios.put(`http://localhost:8080/Students/${user.ID}`,final,{headers: { contentType : 'application/json' }})
+              setUser({ID:"",firstName:"", lastName :"",dateOfBirth:"", email :"" })
+               getdata();
+               getdata(); 
+          }
        
 
-    return (
 
+           
+    return (
+      
+       <>
         <div id="card">
         <div id="card-content">
           <div id="card-title">
             <h2>User Information</h2>
             <div class="underline-title"></div>
           </div>
-          <div class="form">
-            
+          <div class="form">            
              <label className='lable1'>First Name  </label>
-           <input id="user-email" class="form-content"  autocomplete="off" required name="fname" value={user.fname} onChange={handleInputs}/>
+           <input id="user-email" class="form-content"  autocomplete="off" required name="firstName" value={user.firstName} onChange={handleInputs}/>
             <div class="form-border"></div>
             
             <label className='lable1'>Last Name</label>            
-            <input id="user-password" class="form-content"   required name="lname" value={user.lname} onChange={handleInputs}/>
+            <input id="user-password" class="form-content"   required name="lastName" value={user.lastName} onChange={handleInputs}/>
             <div class="form-border"></div>
             
             <label className='lable1'>Date of birth  </label>
-           <input id="user-email" class="form-content"  autocomplete="off" required name="bday" value={user.bday} onChange={handleInputs}/>
-           <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+            <input  type="date" id="user-email" class="form-content"  autocomplete="off" required name="dateOfBirth" value={user.dateOfBirth} onChange={handleInputs}/>
+          
             <div class="form-border"></div>
             
-            <label className='lable1'>Email  </label>
+            <label className='lable1'>Email </label>
            <input id="user-email" class="form-content"  autocomplete="off" required name="email" value={user.email} onChange={handleInputs}/>
             <div class="form-border"></div>
+           {
+             toggle ?<input id="submit-btn" type="submit" name="submit" value="SAVE"  onClick={postData}/>:
            
-            <input id="submit-btn" type="submit" name="submit" value="SAVE" onClick={postData}  />  
-             
+              <input id="submit-btn" type="submit" name="submit" value="UPDATE"  onClick={updateUser}/>
+             }  
+         
           </div>
         </div>
       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // <div className='card'>
-        //  <div>  First Name : 
-        //    <input type="text"></input> 
-        //    </div>
-        //    <div>  Last Name : 
-        //    <input type="text"></input> 
-        //    </div>
-        //    <div>  Date of birth : 
-        //    <input type="text"></input> 
-        //    </div>
-        //    <div>  Email : 
-        //    <input type="text"></input> 
-        //    </div>
-        // </div>
+<div className='rightframe'>
+<div className='rowtitle'>
+  <ul>
+  <li>First name</li>
+    <li>Last name</li>
+    <li>Date of birth</li>
+    <li>Email of User</li>
+    {/* <li className='icon' onClick={()=>{deleteUser("Abhinav")}}><IoTrashOutline/></li> */}
+    <button >---------------Actions--------------</button> 
+    </ul></div>
+  
+  { allUser.map((itemval)=>{  
+    return ( <ul className='newlist'>
+    <li> {itemval.firstName}</li>
+    <li> {itemval.lastName}</li>
+    <li> {itemval.dateOfBirth}</li>
+    <li> {itemval.email}</li>
+   
+    <li className='icon'  onClick={()=>{deleteUser(itemval._id)}}><IoTrashOutline/></li> 
+     <li className='icon' onClick={()=>{ FillUserForm(itemval._id,itemval.firstName,itemval.lastName,itemval.dateOfBirth,itemval.email)}}><FiEdit/></li>
+     
+       </ul>  
+         )
+           })  } 
+    </div>
+   </>    
     )
 }
 
